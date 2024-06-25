b9bae0c297fc8d933c93479ccdd336c1a0d4b8c6
Tatsiana <mokosiejtatsiana@gmail.com>
2024-06-25 17:13:36 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
-public static void main(String[] args) {
-    SearchQueryCounter counter = new SearchQueryCounter();
-    Scanner scanner = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        SearchQueryCounter counter = new SearchQueryCounter();
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Введите поисковый запрос (или \"exit\" для выхода):");
-    while (true) {
-        String query = scanner.nextLine();
-        if (query.equalsIgnoreCase("exit")) {
-            break;
+        System.out.println("Введите поисковый запрос (или \"exit\" для выхода):");
+        while (true) {
+            String query = scanner.nextLine();
+            if (query.equalsIgnoreCase("exit")) {
+                break;
+            }
+            int count = counter.search(query);
+            System.out.printf("Запрос \"%s\" был выполнен %d раз(а)%n", query, count);
         }
-        counter.search(query);
-    }
+
+        String testQuery = "тест";
+        for (int i = 0; i < 105; i++) {
+            int count = counter.search(testQuery);
+            System.out.printf("Запрос \"%s\" был выполнен %d раз(а)%n", testQuery, count);
+        }
 
 
-    String testQuery = "тест";
-    for (int i = 0; i < 105; i++) {
-        counter.search(testQuery);
     }
 }
--- src/SearchQueryCounter.java
+++ src/SearchQueryCounter.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class SearchQueryCounter {
     private static final int MAX_COUNT = 100;
     private Map<String, Integer> searchCounts;
@@ -10,16 +9,14 @@ public class SearchQueryCounter {
         searchCounts = new HashMap<>();
     }
 
-    public void search(String query) {
-        if (!searchCounts.containsKey(query)) {
-            searchCounts.put(query, 0);
-        }
+    public int search(String query) {
+        searchCounts.putIfAbsent(query, 0);
 
         int currentCount = searchCounts.get(query);
         if (currentCount < MAX_COUNT) {
             searchCounts.put(query, currentCount + 1);
         }
 
-        System.out.printf("Запрос \"%s\" был выполнен %d раз(а)%n", query, searchCounts.get(query));
+        return searchCounts.get(query);
     }
 }
